f219539cefa66b7bb731b8bb23ad4939223a6a63
VIBHA <artvibha2007@gmail.com>
2026-03-09 19:55:19 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,35 @@
+const careerSkills = {
+
+"Data Scientist":[
+"Python",
+"Machine Learning",
+"Statistics",
+"SQL",
+"Data Visualization"
+],
+
+"Web Developer":[
+"HTML",
+"CSS",
+"JavaScript",
+"React",
+"Node.js"
+],
+
+"AI Engineer":[
+"Python",
+"Deep Learning",
+"TensorFlow",
+"Machine Learning",
+"NLP"
+],
+
+"Cybersecurity Analyst":[
+"Networking",
+"Linux",
+"Ethical Hacking",
+"Security Analysis",
+"Cryptography"
+]
+
+}
