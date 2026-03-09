bc3afa287070d64bb4f8e3d78c4ddb6108ece1bb
VIBHA <artvibha2007@gmail.com>
2026-03-09 20:19:54 +0530

    Update data.js

--- data.js
+++ data.js
@@ -33,3 +33,45 @@ const careerSkills = {
 ]
 
 }
+const courseData = {
+
+"Python":[
+"Python for Everybody - Coursera",
+"Complete Python Bootcamp - Udemy"
+],
+
+"Machine Learning":[
+"Machine Learning by Andrew Ng - Coursera",
+"Machine Learning Specialization - DeepLearning.AI"
+],
+
+"SQL":[
+"SQL for Data Science - Coursera",
+"The Complete SQL Bootcamp - Udemy"
+],
+
+"Data Visualization":[
+"Data Visualization with Tableau - Coursera"
+],
+
+"HTML":[
+"HTML & CSS Crash Course - freeCodeCamp"
+],
+
+"CSS":[
+"Responsive Web Design - freeCodeCamp"
+],
+
+"JavaScript":[
+"JavaScript Algorithms - freeCodeCamp"
+],
+
+"React":[
+"React Developer Course - Udemy"
+],
+
+"Node.js":[
+"Node.js Complete Guide - Udemy"
+]
+
+}
